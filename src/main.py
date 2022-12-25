1ff03da31c84ad720f43f92c27cd2a1bc40a6d51
Daniel Luna Cienfuegos <juandaniel.lunacienfuegos@gmail.com>
2022-12-24 23:14:52 -0600

    Bot v1.0.1

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
-from commands import jokes,ping
+from commands import jokes, ping
 
 intents = discord.Intents.all()
 
-bot = commands.Bot("s!", intents= intents)
+bot = commands.Bot("s!", intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -12,15 +13,16 @@ async def on_ready():
     await bot.change_presence(activity=activity)
     print(f"{bot.user}> is logged on")
 
+
 @bot.command(name="chiste")
 async def __jokes(ctx):
     await jokes(ctx)
 
+
 @bot.command(name="ping")
 async def __ping(ctx):
-    message = ctx.message
-    await message.delete()
     await ping(ctx, bot)
 
 
-bot.run(token="") # Añadir tokend
+# Añadir token
+bot.run(token="")
