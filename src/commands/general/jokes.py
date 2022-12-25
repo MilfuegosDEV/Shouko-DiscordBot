bec876c418bbfb550929a1131a5d3bd4bdb95d93
Daniel Luna Cienfuegos <juandaniel.lunacienfuegos@gmail.com>
2022-12-24 23:11:29 -0600

    s!chistes v2.0.1

--- src/commands/general/jokes.py
+++ src/commands/general/jokes.py
@@ -1,12 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import discord
+from discord.ext import commands
+from datetime import datetime
+
 
 def __get_random_chiste():
     response = requests.get('http://www.chistes.com/ChisteAlAzar.asp?n=3')
     soup = BeautifulSoup(response.text, "html.parser")
     chiste = soup.find("div", "chiste").text
-    
+
     return chiste
 
-def jokes(ctx):
-    return ctx.send("<@{}> {}".format(ctx.author.id, __get_random_chiste()))
+
+def jokes(ctx: commands.Bot):
+    # embed
+    embed = discord.Embed(description=__get_random_chiste())
+
+    # footer
+    embed.set_footer(text="v2.0.1 • Hoy a las {}".format(
+        datetime.today().strftime('%H:%M')))
+
+    return ctx.reply(embed=embed)
