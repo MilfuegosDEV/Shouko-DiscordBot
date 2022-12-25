339a398107e5ff73cbe9090348d3f574e7c45e77
Daniel Luna Cienfuegos <juandaniel.lunacienfuegos@gmail.com>
2022-12-24 23:14:40 -0600

    s!ping v2.0.1

--- src/commands/general/ping.py
+++ src/commands/general/ping.py
@@ -1,2 +1,14 @@
-def ping(ctx, bot):
-    return ctx.send(f"<@{ctx.author.id}> **Pong!** :ping_pong:\n:incoming_envelope: {round(bot.latency * 1000, 0)} ***ms***")
+import discord
+from datetime import datetime
+from discord.ext import commands
+
+
+def ping(ctx, bot: commands.Bot):
+    # embed
+    embed = discord.Embed(
+        description=f":ping_pong: **Pong!**\n:incoming_envelope: Mensajes: {int(round(bot.latency * 1000, 0))}ms")
+    # footer
+    embed.set_footer(text="v2.0.1 • Hoy a las {}".format(
+        datetime.today().strftime('%H:%M')))
+
+    return ctx.reply(embed=embed)
