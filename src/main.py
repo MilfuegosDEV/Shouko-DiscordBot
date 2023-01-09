7c417065707f6d6dfbf10c871557c9f6be652761
Daniel Luna Cienfuegos <juandaniel.lunacienfuegos@gmail.com>
2023-01-08 22:48:15 -0600

    main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,22 @@ import discord
 from discord.ext import commands
 from commands import jokes, ping
 from games import typing
-def token(path: str):
-    """Requiere únicamente un parametro el cuál es la ruta donde se encuentra guardado el token, incluyendo la extensión de dicho archivo.\n\nRetorna el token."""
+
+
+def token(path: str) -> str:
+    """
+    Lee el token del bot de un archivo de texto y lo devuelve.
+
+    Parameters:
+    path (str): La ruta del archivo que contiene el token, incluyendo la extensión del archivo.
+
+    Returns:
+    str: El token del bot.
+    """
     with open(path, "r") as file:
         token = file.read()
     return token
+
 
 intents = discord.Intents.all()
 
@@ -15,6 +26,7 @@ bot = commands.Bot("s!", intents=intents)
 
 @bot.event
 async def on_ready():
+    """Muestra un mensaje cuando el bot se conecta al servidor y cambia la actividad del perfil del bot."""
     activity = discord.activity.Game("s!help - más info")
     await bot.change_presence(activity=activity)
     print(f"{bot.user}> is logged on")
@@ -22,16 +34,20 @@ async def on_ready():
 
 @bot.command(name="chiste")
 async def __jokes(ctx):
+    """Muestra un chiste aleatorio en el canal de Discord."""
     await jokes(ctx)
 
 
 @bot.command(name="ping")
 async def __ping(ctx):
+    """Muestra la latencia del bot en el canal de Discord."""
     await ping(ctx, bot)
+
 
 @bot.command(name="type")
 async def __type(ctx):
+    """Realiza un test de escritura en el canal de Discord y muestra los resultados."""
     await typing(ctx, bot)
 
-# Añadir token
+# Conecta el bot al servidor de Discord
 bot.run(token=token(path="token.txt"))
